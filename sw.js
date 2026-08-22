b53c06313a12b2bc9dfe6842ac922e73a7bd03bc
Manuel Wolber <manuelwolber1@gmail.com>
2026-08-22 09:54:15 +0200

    Bump SW cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fight-log-v4";
+const CACHE = "fight-log-v5";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
