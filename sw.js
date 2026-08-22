85c8dd91957d24056870672e0501fe37e3ef0ec3
Manuel Wolber <manuelwolber1@gmail.com>
2026-08-22 19:29:53 +0200

    Add Profile: Personal Records + Fight Record (8 categories) inside Stats page

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fight-log-v6";
+const CACHE = "fight-log-v7";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
