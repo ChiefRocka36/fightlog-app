df13f0b38eec5fafe24c7b70010f9df83dec39a2
Manuel Wolber <manuelwolber1@gmail.com>
2026-08-24 14:04:34 +0200

    Add Goals (Weekly/Monthly/Life) as a compact collapsible Stats card, progress auto-computed from logged sessions, handles multiple sessions per day

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fight-log-v11";
+const CACHE = "fight-log-v12";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
